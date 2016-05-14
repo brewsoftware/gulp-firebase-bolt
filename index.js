8226375628f61318602df4b46c03a7f42e732380
Jason Turner <jturner@brewsoftware.com.au>
2016-05-14 17:30:21 +1000

    initial checkin

---
+++ index.js
@@ -0,0 +1,10 @@
+/*
+
+  Firebase bolt gulp plugin
+  Note: Using a modified bolt syntax to include imports
+
+*/
+
+module.exports = function(destination, opts) {
+    // run whatever commands we need to in here.
+};
