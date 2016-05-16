d856b8fc10afe7a8f8bb44e83c893fe5ac45ea94
Jason Turner <jturner@brewsoftware.com.au>
2016-05-16 23:41:16 +1000

    Added test processing rules.

--- index.js
+++ index.js
@@ -15,14 +15,13 @@ module.exports = function() {
     // creating a stream through which each file will pass
       var stream = through.obj(function(file, enc, cb) {
         var self = this;
-        var parsedPath = parsePath(file.relative);
         var newfile = file.clone({contents: false});
         var content ='';
         if (file.isBuffer()) {
 
           content = file.contents.toString()
-          parserWrapper(content,parsedPath).then(function(symbols){
-            readSuccess(symbols, cb,newfile,parsedPath );
+          parserWrapper(content).then(function(symbols){
+            readSuccess(symbols, cb,newfile );
           }).catch(function(ex){
              this.emit('error', new PluginError(PLUGIN_NAME, 'Error converting file'));
              cb();
@@ -33,7 +32,9 @@ module.exports = function() {
           });
           file.contents.on('end', function(){
             // make sure the file goes through the next gulp plugin
-            parserWrapper(content,parsedPath ).then(function(symbols){
+            parserWrapper(content ).then(function(symbols){
+              console.log("!!!!!!!!!");
+              console.log(symbols);
               readSuccess(symbols, cb,newfile );
             }).catch(function(ex){
                this.emit('error', new PluginError(PLUGIN_NAME, 'Error converting file'));
@@ -65,41 +66,96 @@ module.exports = function() {
          extname: extname
        };
      }
-    function parserWrapper(data,parsedPath ) {
-        var promises = [];
-        var sym = bolt.parse(data);
-        while (sym.imports.length > 0) {
-            var next = sym.imports.pop();
-            var p = readfile( next.filename + '.bolt').then(
-              function(subData){
-                  console.log('data');
-                  console.log(subData.toString());
-              var newRules = bolt.parse(subData.toString());
-              if (newRules) {
-                  newRules.imports.map(function (obj) {
-                      sym.imports.push(obj);
-                      return obj;
-                  });
-              }
-              for (var funcKey in newRules.functions) {
-                  if (newRules.functions.hasOwnProperty(funcKey)) {
-                      sym.functions[funcKey] = newRules.functions[funcKey];
-                  }
-              }
-              for (var schemaKey in newRules.schema) {
-                  if (newRules.schema.hasOwnProperty(schemaKey)) {
-                      sym.schema[schemaKey] = newRules.schema[schemaKey];
-                  }
-              }
-            });
-            promises.push(p);
-        }
-        var retPromise = new Promise(function(resolve, reject){
-          Promise.all(promises).then(function(){
-            resolve(sym);
-          });
-        });
-        return retPromise;
-    };
+     var sym;
+    function parserWrapper(data) {
+      var promises = [];
+      sym = bolt.parse(data);
+      while (sym.imports.length > 0) {
+          var next = sym.imports.pop();
+          var p = processRecursive(next);
 
+          promises.push(p);
+      } // end while
+      var retPromise = new Promise(function(resolve, reject){
+        Promise.all(promises).then(function(){
+          console.log("sym:");
+          console.log(sym);
+          resolve(sym);
+        }).catch(function(ex){
+          console.log(ex);
+        });
+      });
+      return retPromise;
+    }; // end function
+
+    function processRecursive(next){
+
+      var promises = [];
+      console.log('next:' + JSON.stringify(next));
+      var p = new Promise(function(resolve, reject) {
+        readfile( next.filename + '.bolt').then(
+          function(subData) {
+            var subPromises = [];
+            console.log('data:'+subData);
+            var newRules = bolt.parse(subData.toString());
+            if (newRules) {
+                newRules.imports.map(function (obj) {
+                    sym.imports.push(obj);
+                    return obj;
+                });
+            }
+            for (var funcKey in newRules.functions) {
+                if (newRules.functions.hasOwnProperty(funcKey)) {
+                    sym.functions[funcKey] = newRules.functions[funcKey];
+                }
+            }
+            for (var schemaKey in newRules.schema) {
+                if (newRules.schema.hasOwnProperty(schemaKey)) {
+                    sym.schema[schemaKey] = newRules.schema[schemaKey];
+                }
+            }
+            /*
+            {
+              filename:
+              alias:
+              scope: fale = local
+            }
+            */
+            console.log('imports:' + JSON.stringify(newRules.imports));
+            for(var impKey in newRules.imports){
+                var imp = newRules.imports[impKey];
+                var relPath = parsePath(next.filename);
+                var thisPath = parsePath(imp.filename);
+                console.log(relPath);
+                console.log(thisPath);
+                //relPath.dirname = relPath.dirname.substring(1,relPath.dirname.length);
+                thisPath.dirname = thisPath.dirname.substring(1,thisPath.dirname.length);
+
+                imp.filename  = relPath.dirname +  thisPath.dirname +"/" + thisPath.basename;
+                console.log("fn:");
+                console.log(imp.filename);
+                // re-write the filename and
+                var inner = processRecursive(imp);
+                subPromises.push(inner);
+                // push a new import path
+            }
+            Promise.all(subPromises).then(function(){
+              resolve();
+            }).carch(function(ex){
+              console.log(ex);
+            });
+
+        }); // end readFile
+      }); // end promise
+    promises.push(p);
+
+    var retPromise = new Promise(function(resolve, reject){
+      Promise.all(promises).then(function(){
+        resolve();
+      }).catch(function(ex){
+        console.log(ex);
+      });
+    });
+    return retPromise;
+    }
 };
